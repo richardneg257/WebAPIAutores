7d3e8d1f80546c0474a745f908451723da1049dc
Richard Negron <richard.negron.257@gmail.com>
2023-04-25 19:52:07 -0500

    comments

--- WebAPIAutores/ApplicationDbContext.cs
+++ WebAPIAutores/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace WebAPIAutores
 
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
--- WebAPIAutores/Entities/Book.cs
+++ WebAPIAutores/Entities/Book.cs
@@ -6,8 +6,10 @@ namespace WebAPIAutores.Entities
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         [FirstCapitalLetter]
         [StringLength(250)]
         public string Title { get; set; }
+        public List<Comment> Comments { get; set; }
     }
 }
